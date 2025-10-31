4613df5a42b0c0595a6072d913c14fb5e550b559
HassanAmir454 <Hassan.Amir@student.lab.fi>
2025-10-31 21:31:04 +0500

    component update

--- src/ChartComponent/ChartComponent.js
+++ src/ChartComponent/ChartComponent.js
@@ -1,40 +1,82 @@
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
+import "./ChartComponent.css"; // We'll style it next
 
 class ChartComponent extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      options: {
-        chart: {
-          id: "basic-bar"
-        },
+      // Chart 1 (Your existing chart)
+      areaOptions: {
+        chart: { id: "basic-bar" },
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-        }
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+        },
       },
-      series: [
+      areaSeries: [
         {
           name: "series-1",
-          data: [30, 40, 45, 50, 49, 60, 70, 91, 85]
-        }
-      ]
+          data: [30, 40, 45, 50, 49, 60, 70, 91, 85],
+        },
+      ],
+
+      // Chart 2 (Bar chart)
+      barOptions: {
+        chart: { id: "bar-chart" },
+        xaxis: { categories: ["Mon", "Tue", "Wed", "Thu", "Fri"] },
+      },
+      barSeries: [
+        { name: "Visitors", data: [120, 200, 150, 80, 70] },
+      ],
+
+      // Chart 3 (Donut chart)
+      donutOptions: {
+        labels: ["Apples", "Bananas", "Oranges", "Grapes"],
+      },
+      donutSeries: [44, 55, 13, 33],
     };
   }
 
   render() {
     return (
-      <div className="chart">
-        <Chart
-          options={this.state.options}
-          series={this.state.series}
-          type="area" // can also be 'bar' or 'line'
-          width="600"
-        />
+      <div className="charts-container">
+        {/* Chart 1 - Area */}
+        <div className="chart-box">
+          <h3>📈 Growth Over Time</h3>
+          <Chart
+            options={this.state.areaOptions}
+            series={this.state.areaSeries}
+            type="area"
+            width="400"
+          />
+        </div>
+
+        {/* Chart 2 - Bar */}
+        <div className="chart-box">
+          <h3>📊 Weekly Visitors</h3>
+          <Chart
+            options={this.state.barOptions}
+            series={this.state.barSeries}
+            type="bar"
+            width="400"
+          />
+        </div>
+
+        {/* Chart 3 - Donut */}
+        <div className="chart-box">
+          <h3>🍩 Fruit Sales</h3>
+          <Chart
+            options={this.state.donutOptions}
+            series={this.state.donutSeries}
+            type="donut"
+            width="400"
+          />
+        </div>
       </div>
     );
   }
 }
 
 export default ChartComponent;
+
